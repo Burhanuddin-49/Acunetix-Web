2c7df52f83f518cc91d267284cb3fb8037c11281
Burhanuddin-49 <Burhan9651@gmail.com>
2023-02-10 15:22:28 +0530

    added meta in doc

--- pages/_document.js
+++ pages/_document.js
@@ -4,6 +4,27 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        <meta
+          property="og:description"
+          content="ACUNETIX is one of the largest events - ACUNETIX 10.0 9TH-11TH 2023"
+        />
+        <meta property="og:title" content="ACUNETIX 10.0, DPU" />
+        <meta property="og:site_name" content="ACUNETIX" />
+        {/* <meta
+          property="og:image"
+          content="https://techfest.org/static/resources/home/website2.png"
+        /> */}
+        <meta property="og:see_also" content="http://acunetix.tech" />
+        <meta name="robots" content="INDEX, FOLLOW" />
+        <meta
+          name="description"
+          content="ACUNETIX is one of the largest events - Acunetix 2022-2023"
+        />
+        <meta
+          name="keyword"
+          content="acunetix, event, dy patil, acunetix 10, game, pimpri, pune,metaverse, vr, ar, , tech, acunetix tech, computer, party, Treasure Trove, UXplorer, Brainiac, DPL, Innovatia, ml ops, gamestorm, Asia,Technology,Science ,Scientific,Tech,Events ,Submerged,aquatic,journey,submarine,plants,Water,memories,Covid,festival,excitement,fantasty art,tech festival copenhangen,tech festival amsterdam,tech festival india,bombay techfest,mumbai,dates,march,9th - 11th,offline,online,Asia's largest science and technology festival,techfest app,Ocean,Multitute,spectacles,glasses,waterfall,volcanoes,deep,sea,giants,bioluminescent,beach,ancient,civilizations,Oceanography,Dr. Gene Feldman,NASA,Space,Mars,animals ,Earth,mystery,realm,sumberged ,submerged sojourn,deep sea,underwater,deep exploration,aquatic,underwater technology, dr dy patil institute of technology, iit, Dr. DY Patil Institute of Technology - Dr. DY Patil Unitech Society, dpu"
+        />
+        <meta name="author" content="Burhanuddin Marcha" />
         <link rel="icon" type="image/png" href="/image/logo.png" />
         <link
           href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css"
