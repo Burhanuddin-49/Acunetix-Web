80e6c07f3a503aeefec82e377c5bc83500d885ed
Burhanuddin-49 <Burhan9651@gmail.com>
2023-02-14 01:56:01 +0530

    update eventdata
    On branch main

--- component/event/eventdata.js
+++ component/event/eventdata.js
@@ -4,15 +4,33 @@ export const data = [
     name: "GAMESTORM",
     date: "9-10 March",
     img: "https://drive.google.com/uc?export=view&id=1cI4tLK2RyyDv0EKRi_J6-S07FyPuOWzK",
-    eventsubline: `Enter the gaming cosmos`,
+    eventsubline: `✨️Enter the gaming cosmos✨️`,
     eventdetail: `A gaming tournament where players compete against one another to display their talents.
     • Lead your SQUAD 😎 
     • Flex your AIM 🎯
     • Get Ready To SLAY🏆
      ɢᴇᴀʀ ᴜᴘ ᴛᴏ ꜱʜᴏᴡ ʏᴏᴜʀ ꜱᴋɪʟʟꜱ ɪɴ :
      --> 🎮 BGMI
+    🔶 ᴘᴀʀᴛɪᴄɪᴘᴀɴᴛꜱ ᴘᴇʀ ᴛᴇᴀᴍ - 4 
+    
     --> 🎮 VALORANT
-    --> 🎮 COUNTER STRIKE`,
+    🔶 Qualifier 1 - Elimination 1
+    🔶 Qualifier 2 - Elimination 2
+    🔶 Finals - Top 2 teams 
+    🔶 ᴘᴀʀᴛɪᴄɪᴘᴀɴᴛꜱ ᴘᴇʀ ᴛᴇᴀᴍ – 5
+    
+    --> 🎮 COUNTER STRIKE
+    🔶 Qualifier 1 - Elimination 1
+    
+    🔶 Qualifier 2 - Elimination 2
+    
+    🔶 Finals - Top 2 teams 
+    
+    🔶 ᴘᴀʀᴛɪᴄɪᴘᴀɴᴛꜱ ᴘᴇʀ ᴛᴇᴀᴍ – 5
+    
+     
+    🔶️ ᴄᴀꜱʜ ᴘʀɪᴢᴇꜱ, ʀᴇᴡᴀʀᴅꜱ & ɢᴏᴏᴅɪᴇꜱ ᴡᴏʀᴛʜ ʀꜱ. 2000 ᴘᴇʀ ɢᴀᴍᴇ
+    `,
     eventlink: "https://forms.gle/t8FqBqWygs1ibxWm9",
   },
   {
@@ -21,9 +39,14 @@ export const data = [
     date: "9 March",
     img: "https://drive.google.com/uc?export=view&id=18aBOKjWLkTiv1P6SOH3e1mA_dkYQ7eX9",
     eventsubline: `💎 Think User, Design Future 💎`,
-    eventdetail: `🔶 ᴀ ᴅᴏᴍᴀɪɴ ʙᴀꜱᴇᴅ ᴄᴏᴍᴘᴇᴛɪᴛɪᴏɴ ᴡʜᴇʀᴇɪɴ ᴛʜᴇ ᴘᴀʀᴛɪᴄɪᴘᴀɴᴛꜱ ᴡɪʟʟ ʜᴀᴠᴇ ᴛᴏ ᴄʀᴇᴀᴛᴇ ᴀ ᴜɪ/ᴜx ᴅᴇꜱɪɢɴ ʙᴀꜱᴇᴅ ᴏɴ ᴛʜᴇ ɢɪᴠᴇɴ ᴛᴏᴘɪᴄ 🧑🏻‍💻👩🏻‍💻
+    eventdetail: `ᴀ ᴅᴏᴍᴀɪɴ-ʙᴀꜱᴇᴅ ᴄᴏɴᴛᴇꜱᴛ ɪɴ ᴡʜɪᴄʜ ᴄᴏᴍᴘᴇᴛɪᴛᴏʀꜱ ᴡɪʟʟ ʙᴇ ɢɪᴠᴇɴ ᴀ ᴘᴀʀᴛɪᴄᴜʟᴀʀ ᴛᴏᴘɪᴄ ᴛᴏ ᴄʀᴇᴀᴛᴇ ꜰʀᴏɴᴛᴇɴᴅ ᴅᴇꜱɪɢɴꜱ ᴜꜱɪɴɢ ʜᴛᴍʟ ᴀɴᴅ ᴄꜱꜱ.
+ 
+    🔶 ᴛʜᴇ ᴘᴀʀᴛɪᴄɪᴘᴀɴᴛꜱ ᴡɪʟʟ ꜱᴏʟᴇʟʏ ʙᴇ ᴊᴜᴅɢᴇᴅ ᴏɴ ᴛʜᴇɪʀ ʀᴇꜱᴘᴇᴄᴛɪᴠᴇ ꜰʀᴏɴᴛᴇɴᴅ ᴅᴇꜱɪɢɴ 🏆
     
-    🔶ᴛʜᴇ ᴘᴀʀᴛɪᴄɪᴘᴀɴᴛꜱ ᴡɪʟʟ ꜱᴏʟᴇʟʏ ʙᴇ ᴊᴜᴅɢᴇᴅ ᴏɴ ᴛʜᴇɪʀ ʀᴇꜱᴘᴇᴄᴛɪᴠᴇ ᴜɪ/ᴜx ᴅᴇꜱɪɢɴ 🏆`,
+    🔶 ᴘᴀʀᴛɪᴄɪᴘᴀɴᴛꜱ ᴘᴇʀ ᴛᴇᴀᴍ – 1
+    
+    🔶️ ʀᴇᴡᴀʀᴅꜱ & ɢᴏᴏᴅɪᴇꜱ ᴡᴏʀᴛʜ 
+    `,
     eventlink: "https://forms.gle/1e3NGWqrBpM1M17U7",
   },
   {
@@ -33,12 +56,15 @@ export const data = [
     img: "https://drive.google.com/uc?export=view&id=1G_9bHYn0a5B4PyxGML4skBYaK5MVQV82",
     eventsubline: `Unravel the mysterious killer 🕵‍♀`,
     eventdetail: `👩🏻‍💻ᴀɴ ɪɴɴᴏᴠᴀᴛɪᴠᴇ ᴄᴏᴍʙɪɴᴀᴛɪᴏɴ ᴏꜰ ᴄᴏᴅɪɴɢ ᴀɴᴅ ᴍʏꜱᴛᴇʀʏ ᴡʜᴇʀᴇɪɴ ʙʏ ꜱᴏʟᴠɪɴɢ ᴄʟᴜᴇꜱ, ᴛʜᴇ ᴘᴀʀᴛɪᴄɪᴘᴀɴᴛꜱ ɢᴇᴛ ᴛᴏ ᴜɴʀᴀᴠᴇʟ ᴛʜᴇ ᴍʏꜱᴛᴇʀɪᴏᴜꜱ ᴋɪʟʟᴇʀ ᴡʜᴏ'ꜱ ʙᴇᴇɴ ʜɪᴅɪɴɢ ᴏɴ ᴛʜᴇɪʀ ᴄᴏᴍᴘᴜᴛᴇʀ ᴛʜɪꜱ ᴡʜᴏʟᴇ ᴛɪᴍᴇ 🕵🏻‍♀
+
+    ᴀ ᴍᴜʀᴅᴇʀ ᴍʏꜱᴛᴇʀʏ ꜱᴛᴏʀʏʟɪɴᴇ ᴡɪᴛʜ ʜɪᴅᴅᴇɴ ᴛᴀꜱᴋꜱ ᴀɴᴅ ʜɪɴᴛꜱ ᴡɪʟʟ ʙᴇ ɢɪᴠᴇɴ ᴛᴏ ᴛʜᴇ ᴘᴀʀᴛɪᴄɪᴘᴀɴᴛ.
+           
+    🔶️ ᴀ ꜱɪɴɢʟᴇ-ʀᴏᴜɴᴅ ᴄᴏᴍᴘᴇᴛɪᴛɪᴏɴ ɪɴ ᴡʜɪᴄʜ ᴛʜᴇ ᴡɪɴɴɪɴɢ ᴛᴇᴀᴍ ᴡɪʟʟ ʙᴇ ᴛʜᴇ ᴏɴᴇ ᴡʜᴏ ꜰɪɴᴅꜱ ᴛʜᴇ ꜰɪɴᴀʟ ᴏʙᴊᴇᴄᴛɪᴠᴇ ꜰɪʀꜱᴛ
     
-    🔶 ᴀ ᴍᴜʀᴅᴇʀ ᴍʏꜱᴛᴇʀʏ ꜱᴛᴏʀʏʟɪɴᴇ ᴡɪᴛʜ ʜɪᴅᴅᴇɴ ᴛᴀꜱᴋꜱ ᴀɴᴅ ʜɪɴᴛꜱ ᴡɪʟʟ ʙᴇ ɢɪᴠᴇɴ ᴛᴏ ᴛʜᴇ ᴘᴀʀᴛɪᴄɪᴘᴀɴᴛ.
+    🔶 ᴘᴀʀᴛɪᴄɪᴘᴀɴᴛꜱ ᴘᴇʀ ᴛᴇᴀᴍ – 1 ᴛᴏ 2
     
-    🔶 ʙʏ ᴄᴏᴍᴘʟᴇᴛɪɴɢ ᴛᴀꜱᴋꜱ ᴡɪᴛʜ ᴛʜᴇ ʜᴇʟᴘ ᴏꜰ ᴄʟᴜᴇꜱ ᴀɴᴅ ʜɪɴᴛꜱ, ᴛʜᴇ ꜰɪɴᴀʟ ᴍᴜʀᴅᴇʀᴇʀ  ꜱʜᴀʟʟ ʙᴇ ꜰᴏᴜɴᴅ ᴏɴ ᴛʜᴇ ᴄᴏᴍᴘᴜᴛᴇʀ 🕵‍♀
-    
-    🔶 ᴘᴀʀᴛɪᴄɪᴘᴀᴛɪᴏɴ ᴄᴀɴ ʙᴇ ᴇɪᴛʜᴇʀ ᴀꜱ ᴀ ꜱᴏʟᴏ ᴏʀ ᴀꜱ ᴀ ᴅᴜᴏ 👩🏻‍💻`,
+    🔶️ ᴄᴀꜱʜ ᴘʀɪᴢᴇꜱ, ʀᴇᴡᴀʀᴅꜱ & ɢᴏᴏᴅɪᴇꜱ ᴡᴏʀᴛʜ- ʀꜱ. 2000
+    `,
     eventlink: "https://forms.gle/EHeU1RxJK4XTBnKU7",
   },
   {
@@ -48,13 +74,17 @@ export const data = [
     img: "https://drive.google.com/uc?export=view&id=13QwNhtsNYgvC06Q5SGJcsQfA27dgyxQE",
     eventsubline: `💫  𝑫𝑹𝑬𝑨𝑴 , 𝑪𝑹𝑬𝑨𝑻𝑬 𝑨𝑵𝑫 𝑳𝑬𝑻’𝑺 𝑰𝑵𝑵𝑶𝑽𝑨𝑻𝑬  💫`,
     eventdetail: `ɪꜰ ʏᴏᴜ ʟᴏᴏᴋ ᴀᴛ ᴛʜᴇ ʜɪꜱᴛᴏʀʏ, ɪɴɴᴏᴠᴀᴛɪᴏɴ ᴅᴏᴇꜱɴ’ᴛ ᴊᴜꜱᴛ ᴄᴏᴍᴇ ꜰʀᴏᴍ ɢɪᴠɪɴɢ ᴘᴇᴏᴘʟᴇ ɪɴᴄᴇɴᴛɪᴠᴇꜱ; ɪᴛ ᴄᴏᴍᴇꜱ ꜰʀᴏᴍ ᴄʀᴇᴀᴛɪɴɢ ᴇɴᴠɪʀᴏɴᴍᴇɴᴛ ᴡʜᴇʀᴇ ᴛʜᴇɪʀ ɪᴅᴇᴀꜱ ᴄᴀɴ ᴄᴏɴɴᴇᴄᴛ💡
-    
-    
-    ⭐ᴛʜᴇ ᴇᴠᴇɴᴛ ᴡɪʟʟ ʜᴀᴠᴇ 2 ʀᴏᴜɴᴅꜱ :
+
+
+    ⭐ ᴛʜᴇ ᴇᴠᴇɴᴛ ᴡɪʟʟ ʜᴀᴠᴇ 2 ʀᴏᴜɴᴅꜱ :
     
     🔶 Round 1:  ɪᴅᴇᴀ ꜱᴜʙᴍɪꜱꜱɪᴏɴ 📑
     
-    🔶 Round 2:  market your ideas, create an advertisement for your product/idea 🎤
+    🔶 Round 2:  ᴍᴀʀᴋᴇᴛ ʏᴏᴜʀ ɪᴅᴇᴀꜱ 💸
+    
+    🔶 ᴘᴀʀᴛɪᴄɪᴘᴀɴᴛꜱ ᴘᴇʀ ᴛᴇᴀᴍ – ꜱᴏʟᴏ ᴏʀ ᴛᴇᴀᴍ
+    
+    🔶️ ᴄᴀꜱʜ ᴘʀɪᴢᴇꜱ, ʀᴇᴡᴀʀᴅꜱ & ɢᴏᴏᴅɪᴇꜱ ᴡᴏʀᴛʜ ʀꜱ. 1000
     `,
     eventlink: "https://forms.gle/Er45GXiqhiW4DWbEA",
   },
@@ -64,9 +94,18 @@ export const data = [
     date: "9-10 March",
     img: "https://drive.google.com/uc?export=view&id=1Q2yiNtpMKksxXfR2jj8jL-80_aLnJHc1",
     eventsubline: `⚡ 𝑪𝒐𝒅𝒆, 𝑪𝒐𝒎𝒑𝒊𝒍𝒆, 𝑪𝒐𝒎𝒑𝒆𝒕𝒆 ⚡`,
-    eventdetail: `🔶 ᴀ ᴄᴏᴅɪɴɢ ʙᴀꜱᴇᴅ ᴄᴏᴍᴘᴇᴛɪᴛɪᴏɴ ɪɴ ᴡʜɪᴄʜ ᴛʜᴇ ᴘᴀʀᴛɪᴄɪᴘᴀɴᴛ ᴡɪʟʟ ᴛᴀᴄᴋʟᴇ ᴇᴀꜱʏ ᴛᴏ ᴄᴏᴍᴘʟᴇx ʟᴇᴠᴇʟ ᴘʀᴏʙʟᴇᴍꜱ ᴛᴏ ᴇɴʜᴀɴᴄᴇ ᴛʜᴇɪʀ ᴄᴏᴅɪɴɢ ꜱᴋɪʟʟꜱ ɪɴ ᴀ ʟɪᴍɪᴛᴇᴅ ᴀᴍᴏᴜɴᴛ ᴏꜰ ᴛɪᴍᴇ ⏳💻
+    eventdetail: `ᴀ ᴄᴏᴅɪɴɢ ʙᴀꜱᴇᴅ ᴄᴏᴍᴘᴇᴛɪᴛɪᴏɴ ɪɴ ᴡʜɪᴄʜ ᴛʜᴇ ᴘᴀʀᴛɪᴄɪᴘᴀɴᴛ ᴡɪʟʟ ᴛᴀᴄᴋʟᴇ ᴇᴀꜱʏ ᴛᴏ ᴄᴏᴍᴘʟᴇx ʟᴇᴠᴇʟ ᴘʀᴏʙʟᴇᴍꜱ ᴛᴏ ᴇɴʜᴀɴᴄᴇ ᴛʜᴇɪʀ ᴄᴏᴅɪɴɢ ꜱᴋɪʟʟꜱ ɪɴ ᴀ ʟɪᴍɪᴛᴇᴅ ᴀᴍᴏᴜɴᴛ ᴏꜰ ᴛɪᴍᴇ ⏳💻
+
+    ᴛʜᴇ ᴘᴀʀᴛɪᴄɪᴘᴀɴᴛꜱ ᴡɪʟʟ ᴄᴏɴᴛᴇɴᴅ ɪɴ ᴛᴡᴏ ʀᴏᴜɴᴅꜱ ᴀɴᴅ ᴄᴏᴍᴘᴇᴛᴇ ᴛᴏ ᴡɪɴ ᴠᴀʀɪᴏᴜꜱ ᴇxᴄɪᴛɪɴɢ ᴘʀɪᴢᴇꜱ. 💰 
     
-    🔶 ᴛʜᴇ ᴘᴀʀᴛɪᴄɪᴘᴀɴᴛꜱ ᴄᴀɴ ᴜꜱᴇ ᴀɴʏ ᴘʀᴏɢʀᴀᴍᴍɪɴɢ ʟᴀɴɢᴜᴀɢᴇ ᴏꜰ ᴛʜᴇɪʀ ᴄʜᴏɪᴄᴇ.👩🏻‍💻`,
+    🔶️ ʀᴏᴜɴᴅ 1: ᴀ ɴᴏʀᴍᴀʟ ᴍᴄQ-ʙᴀꜱᴇᴅ ʀᴏᴜɴᴅ ᴡʜᴇʀᴇ ᴛʜᴇ ᴘᴀʀᴛɪᴄɪᴘᴀɴᴛꜱ ᴡɪʟʟ ʙᴇ ɢɪᴠᴇɴ ꜱᴏᴍᴇ ʙᴀꜱɪᴄ ᴄᴏᴅᴇ ꜱɴɪᴘᴘᴇᴛꜱ.
+    
+    🔶️ ʀᴏᴜɴᴅ 2: ᴀ ᴘʀᴏᴘᴇʀ ᴄᴏᴅɪɴɢ ʀᴏᴜɴᴅ ᴡɪᴛʜ ꜰᴇᴡ Qᴜᴇꜱᴛɪᴏɴꜱ ᴛᴏ ꜱᴏʟᴠᴇ. ᴛʜᴇ ᴘᴀʀᴛɪᴄɪᴘᴀɴᴛꜱ ᴄᴀɴ ᴜꜱᴇ ᴀɴʏ ᴘʀᴏɢʀᴀᴍᴍɪɴɢ ʟᴀɴɢᴜᴀɢᴇ ᴏꜰ ᴛʜᴇɪʀ ᴄʜᴏɪᴄᴇ. 👩🏻‍💻
+    
+    🔶 ᴘᴀʀᴛɪᴄɪᴘᴀɴᴛꜱ ᴘᴇʀ ᴛᴇᴀᴍ – 1
+    
+    🔶️ ᴄᴀꜱʜ ᴘʀɪᴢᴇꜱ, ʀᴇᴡᴀʀᴅꜱ & ɢᴏᴏᴅɪᴇꜱ ᴡᴏʀᴛʜ - ʀꜱ. 3000
+    `,
 
     eventlink: "https://forms.gle/XCcDvSUHYmeyxYtJ9",
   },
@@ -78,21 +117,17 @@ export const data = [
     eventsubline: ` 𝐃.𝐘. 𝐏𝐀𝐓𝐈𝐋 𝐏𝐑𝐄𝐌𝐈𝐄𝐑 𝐋𝐄𝐀𝐆𝐔𝐄 `,
     tagline: `𝑨 𝑮𝒂𝒎𝒆 𝑶𝒇 𝑺𝒌𝒊𝒍𝒍 & 𝑺𝒕𝒓𝒂𝒕𝒆𝒈𝒚`,
     eventdetail: `🌟 ᴛʜᴇ ᴇᴠᴇɴᴛ ᴡɪʟʟ ʜᴀᴠᴇ ᴛʜʀᴇᴇ ʀᴏᴜɴᴅꜱ:
+ 
+    🔶 Round 1 :  𝑸𝑼𝑰𝒁 𝑹𝑶𝑼𝑵𝑫 𝑶𝑵 𝑮𝑬𝑵𝑬𝑹𝑨𝑳 𝑪𝑹𝑰𝑪𝑲𝑬𝑻
     
-    🔶 Round 1 :  𝑸𝑼𝑰𝒁 𝑹𝑶𝑼𝑵𝑫
-     
-    ᴄᴏᴍᴇ ᴛᴇsᴛ ʏᴏᴜʀ ᴋɴᴏᴡʟᴇᴅɢᴇ ᴏɴ ᴄʀɪᴄᴋᴇᴛ
+    🔶 Round 2 :  𝑸𝑼𝑰𝒁 𝑹𝑶𝑼𝑵𝑫 + 𝟑 𝑷𝑳𝑨𝒀𝑬𝑹 𝑺𝑬𝑳𝑬𝑪𝑻𝑰𝑶𝑵 𝑷𝑶𝑶𝑳
     
-    🔶 Round 2 :  TEST PANEL
+    🔶 Round 3 :  𝑰𝑷𝑳 𝑨𝑼𝑪𝑻𝑰𝑶𝑵
     
-    test panel consists of quiz + selection of 3 players
+    🔶 ᴘᴀʀᴛɪᴄɪᴘᴀɴᴛꜱ ᴘᴇʀ ᴛᴇᴀᴍ – 1 to 3
     
-    🔶 Round 3 :  𝑲𝑵𝑶𝑾 𝒀𝑶𝑼𝑹 𝑻𝑬𝑨𝑴
-    
-    ᴡᴏʀᴋ ᴛᴏɢᴇᴛʜᴇʀ ᴀɴᴅ ᴜᴛɪʟɪᴢᴇ ʏᴏᴜʀ ᴛᴇᴀᴍ ᴋɴᴏᴡʟᴇᴅɢᴇ ɪɴ ᴏʀᴅᴇʀ ᴛᴏ ᴀɴsᴡᴇʀ Qᴜᴇsᴛɪᴏɴs
-    Qᴜɪᴄᴋʟʏ
-    
-    🏆 wɪɴɴɪɴɢ ᴛᴇᴀᴍs ᴡɪʟʟ ʙᴇ ʀᴇᴡᴀʀᴅᴇᴅ ᴡɪᴛʜ ᴄᴀsʜ ᴘʀɪᴢᴇs 💰🤑`,
+    🔶️ ᴄᴀꜱʜ ᴘʀɪᴢᴇꜱ, ʀᴇᴡᴀʀᴅꜱ & ɢᴏᴏᴅɪᴇꜱ ᴡᴏʀᴛʜ- ʀꜱ. 2000 & ᴀ ᴛʀᴏᴘʜʏ
+    `,
     eventlink: "https://forms.gle/68nTsa7VHkpLukP66",
   },
   {
@@ -101,21 +136,21 @@ export const data = [
     date: "10 March",
     img: "https://drive.google.com/uc?export=view&id=1rFKlcAlQ5GYwh5tIWkB6DVP74CPpBEXc",
     eventsubline: `𝑴𝒂𝒄𝒉𝒊𝒏𝒆 𝒍𝒆𝒂𝒓𝒏𝒊𝒏𝒈 𝒘𝒊𝒍𝒍 𝒃𝒆 𝒇𝒐𝒓 𝒚𝒐𝒖 𝒘𝒉𝒂𝒕 𝒚𝒐𝒖 𝒘𝒂𝒏𝒕 𝒊𝒕 𝒕𝒐 𝒃𝒆`,
-    eventdetail: `🔸 ᴀ ᴄᴏᴍᴘᴇᴛɪᴛɪᴏɴ ᴛʜᴀᴛ ᴀɪᴍꜱ ᴛᴏ ᴇᴠᴀʟᴜᴀᴛᴇ ᴋɴᴏᴡʟᴇᴅɢᴇ ᴀɴᴅ ꜱᴋɪʟʟꜱ ᴏꜰ ᴘᴀʀᴛɪᴄɪᴘᴀɴᴛꜱ ɪɴ ᴛʜᴇ ᴍᴀᴄʜɪɴᴇ ʟᴇᴀʀɴɪɴɢ ᴅᴏᴍᴀɪɴ 💻
-    
-    🔸ᴛʜᴇ ᴡɪɴɴɪɴɢ ᴛᴇᴀᴍ ᴡɪʟʟ ʙᴇ ᴅᴇᴛᴇʀᴍɪɴᴇᴅ ʙʏ ɪᴛꜱ ꜰɪɴᴀʟ ꜱᴄᴏʀᴇ. ᴛᴏᴛᴀʟ ᴘᴏɪɴᴛꜱ ᴄᴏʟʟᴇᴄᴛᴇᴅ ɪɴ ʀᴏᴜɴᴅꜱ 1 ᴀɴᴅ 2 ᴡᴏᴜʟᴅ ᴅᴇᴛᴇʀᴍɪɴᴇ ᴛʜᴇ ꜰɪɴᴀʟ ꜱᴄᴏʀᴇ 📈
+    eventdetail: `ᴀ ᴄᴏᴍᴘᴇᴛɪᴛɪᴏɴ ᴛʜᴀᴛ ᴀɪᴍꜱ ᴛᴏ ᴇᴠᴀʟᴜᴀᴛᴇ ᴛᴇᴄʜɴɪᴄᴀʟ, ᴛʜᴇᴏʀᴇᴛɪᴄᴀʟ ᴀɴᴅ ɪɴᴛᴜᴛɪᴏɴ ᴏꜰ ᴘᴀʀᴛɪᴄɪᴘᴀɴᴛꜱ ɪɴ ᴛʜᴇ ᴍᴀᴄʜɪɴᴇ ʟᴇᴀʀɴɪɴɢ ᴅᴏᴍᴀɪɴ 💻
+
+    ᴛᴏᴛᴀʟ ᴘᴏɪɴᴛꜱ ᴄᴏʟʟᴇᴄᴛᴇᴅ ɪɴ ʀᴏᴜɴᴅꜱ 1 ᴀɴᴅ 2 ᴡᴏᴜʟᴅ ᴅᴇᴛᴇʀᴍɪɴᴇ ᴛʜᴇ ꜰɪɴᴀʟ ꜱᴄᴏʀᴇ 📈
     
     ⭐ᴛʜᴇ ᴇᴠᴇɴᴛ ᴡɪʟʟ ʜᴀᴠᴇ 2 ʀᴏᴜɴᴅꜱ :
     
-    🔸 𝙍𝙤𝙪𝙣𝙙 1:  𝑴𝑪𝑸 𝑻𝑬𝑺𝑻
+    🔶 𝙍𝙤𝙪𝙣𝙙 1:  𝑴𝑪𝑸 𝑻𝑬𝑺𝑻
+    ɴᴏ ᴄᴏᴅɪɴɢ Qᴜᴇꜱᴛɪᴏɴꜱ ᴡɪʟʟ ʙᴇ ᴀꜱᴋᴇᴅ, ᴏɴʟʏ ᴛʜᴇ ᴜɴᴅᴇʀꜱᴛᴀɴᴅɪɴɢ ᴀɴᴅ ɪɴᴛᴜɪᴛɪᴏɴ ᴡɪʟʟ ʙᴇ ᴛᴇꜱᴛᴇᴅ.
     
-    A machine learning quiz where the knowledge of the participant will be tested.
-    No coding questions will be asked, only the understanding and intuition will be tested.
+    🔶 𝙍𝙤𝙪𝙣𝙙 2 :  𝑪𝑶𝑫𝑬 𝑪𝑯𝑨𝑳𝑳𝑬𝑵𝑮𝑬
+    ᴀ ᴅᴀᴛᴀꜱᴇᴛ ᴡɪᴛʜ ᴘᴀʀᴛɪᴀʟ ᴄᴏᴅᴇ ᴡɪʟʟ ʙᴇ ɢɪᴠᴇɴ ᴀɴᴅ ᴛʜᴇ ᴘᴀʀᴛɪᴄɪᴘᴀɴᴛ ᴡɪʟʟ ʜᴀᴠᴇ ᴛᴏ ᴏᴘᴛɪᴍɪᴢᴇ ᴛʜᴇ ᴍᴏᴅᴇʟ.
     
-    🔸 𝙍𝙤𝙪𝙣𝙙 2 :  𝑪𝑶𝑫𝑬 𝑪𝑯𝑨𝑳𝑳𝑬𝑵𝑮𝑬
+    🔶 ᴘᴀʀᴛɪᴄɪᴘᴀɴᴛꜱ ᴘᴇʀ ᴛᴇᴀᴍ – 1 to 2
     
-    A dataset with partial code will be given and the participant will have to optimize the model with whatever methodology they wish. 
-Limited Internet access will be provided for help during the competition.
+    🔶️ ᴄᴀꜱʜ ᴘʀɪᴢᴇꜱ, ʀᴇᴡᴀʀᴅꜱ & ɢᴏᴏᴅɪᴇꜱ - ʀꜱ. 1000
     `,
     eventlink: "https://forms.gle/p981byHmGTW9VLGM8",
   },
@@ -124,24 +159,25 @@ Limited Internet access will be provided for help during the competition.
     name: "BRAINIAC",
     date: "10 March",
     img: "https://drive.google.com/uc?export=view&id=1VE_RdJxRoVpNYLkKnYH8QSnJE_r6ePWt",
-    eventsubline: `🏵️ 𝑨𝑰𝑴 𝑭𝑶𝑹 𝑷𝑹𝑶𝑮𝑹𝑬𝑺𝑺 🏵️`,
+    eventsubline: `🏵️ 𝑼𝑵𝑳𝑬𝑨𝑺𝑯 𝒀𝑶𝑼𝑹 𝑰𝑵𝑻𝑬𝑹𝑽𝑰𝑬𝑾𝑰𝑵𝑮 𝑺𝑲𝑰𝑳𝑳𝑺 🏵️`,
     eventdetail: `ʙʀᴇᴀᴋ ᴏᴜᴛ ᴏꜰ ʏᴏᴜʀ ᴄᴏᴍꜰᴏʀᴛ ᴢᴏɴᴇ ᴀɴᴅ ᴅɪꜱᴄᴏᴠᴇʀ ᴛʜᴇ ᴏᴘᴘᴏʀᴛᴜɴɪᴛɪᴇꜱ ɴᴏᴡ ʙᴇᴄᴀᴜꜱᴇ ᴛʀᴜᴇ ᴀᴄʜɪᴇᴠᴇᴍᴇɴᴛꜱ ᴄᴏᴍᴇ ʙʏ ᴛᴀᴋɪɴɢ ᴏɴᴇ ꜱᴍᴀʟʟ ꜱᴛᴇᴘ ᴀᴛ ᴀ ᴛɪᴍᴇ.
-    
+
     ɪᴛ'ꜱ ᴛɪᴍᴇ ᴛᴏ ᴘᴜᴛ ʏᴏᴜʀ ꜱᴋɪʟʟꜱ ᴏɴ ᴅɪꜱᴘʟᴀʏ ᴀꜱ ʙʀᴀɪɴɪᴀᴄ ᴀɪᴍꜱ ᴛᴏ ᴘʀᴇᴘᴀʀᴇ ʏᴏᴜ ꜰᴏʀ ᴛʜᴇ ᴘʟᴀᴄᴇᴍᴇɴᴛ ᴘʀᴏᴄᴇꜱꜱ ɪɴᴄʟᴜᴅɪɴɢ ᴘʀᴇʟɪᴍɪɴᴀʀʏ ᴇxᴀᴍɪɴᴀᴛɪᴏɴꜱ, ɢʀᴏᴜᴘ ᴅɪꜱᴄᴜꜱꜱɪᴏɴꜱ ᴀɴᴅ ʜʀ ɪɴᴛᴇʀᴠɪᴇᴡꜱ 👩‍💻🧑‍💻
     
-    ⭐ᴛʜᴇ ᴇᴠᴇɴᴛ ᴡɪʟʟ ʜᴀᴠᴇ 3 ʀᴏᴜɴᴅꜱ :
+    ᴘᴀʀᴛɪᴄɪᴘᴀɴᴛꜱ ᴘᴇʀ ᴛᴇᴀᴍ – 1
+    
+    ⭐ ᴛʜᴇ ᴇᴠᴇɴᴛ ᴡɪʟʟ ʜᴀᴠᴇ 3 ʀᴏᴜɴᴅꜱ :
     
     🔶 Round 1 : ᴀᴘᴛɪᴛᴜᴅᴇ ᴛᴇꜱᴛ 
     
-    ᴄʜᴇᴄᴋ ʏᴏᴜʀ ꜱᴘᴇᴇᴅ ᴀɴᴅ ᴀᴄᴄᴜʀᴀᴄʏ
-    
     🔶 Round 2 : ɢʀᴏᴜᴘ ᴅɪꜱᴄᴜꜱꜱɪᴏɴ
-    
-    ᴄʜᴇᴄᴋ ʏᴏᴜʀ ᴛʜɪɴᴋɪɴɢ, ʟɪꜱᴛᴇɴɪɴɢ ᴀɴᴅ ꜱᴘᴇᴀᴋɪɴɢ ꜱᴋɪʟʟꜱ
     
     🔶 Round 3 : ᴍᴏᴄᴋ ɪɴᴛᴇʀᴠɪᴇᴡ 
     
-    ᴘʀᴇᴘᴀʀᴇ ʏᴏᴜʀꜱᴇʟꜰ ꜰᴏʀ ɪɴᴛᴇʀᴠɪᴇᴡ`,
+    🔶 ᴘᴀʀᴛɪᴄɪᴘᴀɴᴛꜱ ᴘᴇʀ ᴛᴇᴀᴍ – 1
+    
+    🔶️ ᴄᴀꜱʜ ᴘʀɪᴢᴇꜱ, ʀᴇᴡᴀʀᴅꜱ & ɢᴏᴏᴅɪᴇꜱ ᴡᴏʀᴛʜ ʀꜱ. 2000
+    `,
     eventlink: "https://forms.gle/Eod7SpGKA13wMHcj6",
   },
   {
@@ -152,12 +188,15 @@ Limited Internet access will be provided for help during the competition.
     eventsubline: `✨ 𝑂𝑛 𝑦𝑜𝑢𝑟 𝑚𝑎𝑟𝑘𝑠, 𝑔𝑒𝑡 𝑠𝑒𝑡, 𝑠𝑛𝑎𝑝! ✨`,
     eventdetail: `🔥 ᴇɴɢᴀɢᴇ  ɪɴ ᴛʜᴇ ᴍᴏꜱᴛ ᴇxᴄɪᴛɪɴɢ ɢᴀᴍᴇ ᴏꜰ ᴘʜᴏᴛᴏɢʀᴀᴘʜʏ, ᴄᴏᴍᴘᴇᴛᴇ  ᴀᴍᴏɴɢꜱᴛ ꜰᴇʟʟᴏᴡ ᴘᴀᴘᴀʀᴀᴢᴢɪ, ᴀɴᴅ ʙᴇ ᴛʜᴇ ꜰɪʀꜱᴛ ᴛᴏ ꜰɪɴᴅ,  ᴄᴀᴘᴛᴜʀᴇ,  ʙᴇᴀᴜᴛɪꜰʏ ᴀɴᴅ ᴘʀᴇꜱᴇɴᴛ  ᴛʜᴇ ꜰɪɴᴀʟ ᴍᴀꜱᴛᴇʀᴘɪᴇᴄᴇ. ☄🥇
 
-
     ⭐ᴛʜᴇ ᴇᴠᴇɴᴛ ᴡɪʟʟ ʜᴀᴠᴇ 2 ʀᴏᴜɴᴅꜱ :
     
-    🔶 Round 1: ᴄʟᴜᴇꜱ ᴡɪʟʟ ʟᴇᴀᴅ ᴛᴏ ᴛʜᴇ ᴛᴏᴘɪᴄ ᴏꜰ ᴛʜᴇ ᴘʜᴏᴛᴏɢʀᴀᴘʜꜱ , ᴡʜɪᴄʜ ᴍᴜꜱᴛ ʙᴇ ᴄʟɪᴄᴋᴇᴅ ᴡɪᴛʜɪɴ ᴛʜᴇ ᴄᴏʟʟᴇɢᴇ ᴄᴀᴍᴘᴜꜱ 🔎 🔎
+    🔶️ Round 1 : ᴄʟᴜᴇ ᴡɪʟʟ ʙᴇ ɢɪᴠᴇɴ ᴛʜᴀᴛ ʟᴇᴀᴅꜱ ᴛᴏ ᴛʜᴇ ᴛᴏᴘɪᴄ ᴏꜰ ᴛʜᴇ ᴘʜᴏᴛᴏ ᴛʜᴀᴛ ɴᴇᴇᴅꜱ ᴛᴏ ʙᴇ ᴛᴀᴋᴇɴ ɪɴ ᴄᴏʟʟᴇɢᴇ ᴄᴀᴍᴘᴜꜱ
     
-    🔶 Round 2: Editting the captured photograph with any free to use software on your device in a given time frame. 👨🏻‍💻
+    🔶️ Round 2 : ᴇᴅɪᴛɪɴɢ ᴛʜᴇ ᴄᴀᴘᴛᴜʀᴇᴅ ᴘʜᴏᴛᴏɢʀᴀᴘʜ ɪɴ ʏᴏᴜʀ ᴏᴡɴ ᴅᴇᴠɪᴄᴇꜱ ᴡɪᴛʜɪɴ ᴛʜᴇ ɢɪᴠᴇɴ ᴛɪᴍᴇ ꜰʀᴀᴍᴇ.
+    
+    🔶 ᴘᴀʀᴛɪᴄɪᴘᴀɴᴛꜱ ᴘᴇʀ ᴛᴇᴀᴍ – 1
+    
+    🔶️ ᴄᴀꜱʜ ᴘʀɪᴢᴇꜱ, ʀᴇᴡᴀʀᴅꜱ & ɢᴏᴏᴅɪᴇꜱ ᴡᴏʀᴛʜ- ʀꜱ. 1000
     `,
     eventlink: "https://forms.gle/6S2VVwFFW3Vz4ApcA",
   },
@@ -172,9 +211,14 @@ Limited Internet access will be provided for help during the competition.
 
   //   ʜᴜɴᴛ ꜰᴏʀ ᴛʜᴇ ᴛʀᴇᴀꜱᴜʀᴇ ᴀɴᴅ ᴡɪɴ ᴇxᴄɪᴛɪɴɢ ᴄᴀꜱʜ ᴘʀɪᴢᴇꜱ 🤑
 
-  //   🟡 ᴘᴀʀᴛɪᴄɪᴘᴀɴᴛꜱ ᴄᴀɴ ᴘʟᴀʏ ᴀꜱ ᴀ ᴛᴇᴀᴍ ᴏꜰ 5 ᴍᴇᴍʙᴇʀꜱ 👥
+  //   ⭐ᴛʜᴇ ᴇᴠᴇɴᴛ ᴡɪʟʟ ʜᴀᴠᴇ 3 ʀᴏᴜɴᴅꜱ :
 
-  //   🔵 ᴛʜᴇ ᴇᴠᴇɴᴛ ᴡɪʟʟ ᴛᴀᴋᴇ ᴘʟᴀᴄᴇ ᴡɪᴛʜɪɴ ᴀɴᴅ ᴏᴜᴛꜱɪᴅᴇ ᴛʜᴇ ᴄᴏʟʟᴇɢᴇ ᴄᴀᴍᴘᴜꜱ 🏫`,
+  //   🔶 Round 1 : 𝑶𝒇𝒇 𝑪𝒂𝒎𝒑𝒖𝒔 𝑯𝒖𝒏𝒕
+
+  //    𝑰𝒏 𝑪𝒂𝒎𝒑𝒖𝒔(𝑼𝒏𝒅𝒆𝒓 𝑪𝒂𝒎𝒑𝒖𝒔 𝑹𝒂𝒅𝒊𝒖𝒔)
+
+  //   🔶 ᴘᴀʀᴛɪᴄɪᴘᴀɴᴛꜱ ᴘᴇʀ ᴛᴇᴀᴍ – 5
+  //   `,
   //   eventlink: "https://forms.gle/Uh4GZSchSdUrpRw39",
   // },
 ];
