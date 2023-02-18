424a46d0f1e895afeb819ab27238da6a9a823932
Burhanuddin-49 <Burhan9651@gmail.com>
2023-02-19 04:09:47 +0530

    added schema org in meta
    On branch main

--- pages/_document.js
+++ pages/_document.js
@@ -1,31 +1,85 @@
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
+  function addProductJsonLd() {
+    return {
+      __html: `{
+      "@context": "https://schema.org/",
+      "@type": "event",
+      "name": "Acunetix",
+      "logo": "https://acunetix.tech/image/logo.png"
+      "url: "https://acunetix.tech"
+      "url: "https://www.acunetix.tech"
+      "image": [
+        "https://drive.google.com/uc?export=view&id=1cI4tLK2RyyDv0EKRi_J6-S07FyPuOWzK",
+        "https://drive.google.com/uc?export=view&id=1G_9bHYn0a5B4PyxGML4skBYaK5MVQV82",
+        "https://drive.google.com/uc?export=view&id=1Q2yiNtpMKksxXfR2jj8jL-80_aLnJHc1"
+      ],
+      "description": "ACUNETIX is one of the largest events | ACUNETIX 10.0 9TH-11TH March 2023.",
+      "event": {
+        "@type": "event",
+        "name": "Acunetix",
+        "url: "https://acunetix.tech/event"
+      },
+      "review": {
+        "@type": "Review",
+        "reviewRating": {
+          "@type": "Rating",
+          "ratingValue": "4",
+          "bestRating": "5"
+        },
+        "author": {
+          "@type": "acunetix",
+          "name": "ACUNETIX is one of the largest events | ACUNETIX 10.0 9TH-11TH March 2023."
+        }
+      },
+      "aggregateRating": {
+        "@type": "AggregateRating",
+        "ratingValue": "4.4",
+        "reviewCount": "89"
+      },
+      "offers": {
+        "@type": "Offer",
+        "url: "https://acunetix.tech/event",
+        "priceCurrency": "INR",
+        "price": "50.0",
+        "priceValidUntil": "2023-03-8",
+      }
+    }
+  `,
+    };
+  }
   return (
     <Html lang="en">
       <Head>
+        <meta property="og:title" content="ACUNETIX 10.0" />
         <meta
           property="og:description"
-          content="ACUNETIX is one of the largest events - ACUNETIX 10.0 9TH-11TH March 2023"
+          content="ACUNETIX is one of the largest events | ACUNETIX 10.0 9TH-11TH March 2023"
         />
-        <meta property="og:title" content="ACUNETIX 10.0" />
         <meta property="og:site_name" content="Acunetix" />
         <meta property="og:url" content="https://acunetix.tech/" />
-        <meta property="og:image" src="/image/poster.jpg" />
-        <meta property="og:see_also" content="http://acunetix.tech" />
+        <meta property="og:image" src="/image/logo.png" />
+        <meta property="og:see_also" content="http://www.acunetix.tech" />
         <meta
           name="description"
           content="ACUNETIX is one of the largest events - Acunetix 2022-2023"
+          key="desc"
         />
         <meta
           name="keyword"
-          content="acunetix, event, dy patil, acunetix 10, game, pimpri, pune,metaverse, vr, ar, , tech, acunetix tech, computer, party, Treasure Trove, UXplorer, Brainiac, DPL, Innovatia, ml ops, gamestorm, Asia,Technology,Science ,Scientific,Tech,Events ,Submerged,aquatic,journey,submarine,plants,Water,memories,Covid,festival,excitement,fantasty art,tech festival copenhangen,tech festival amsterdam,tech festival india,bombay techfest,mumbai,dates,march,9th - 11th,offline,online,Asia's largest science and technology festival,techfest app,Ocean,Multitute,spectacles,glasses,waterfall,volcanoes,deep,sea,giants,bioluminescent,beach,ancient,civilizations,Oceanography,Dr. Gene Feldman,NASA,Space,Mars,animals ,Earth,mystery,realm,sumberged ,submerged sojourn,deep sea,underwater,deep exploration,aquatic,underwater technology, dr dy patil institute of technology, iit, Dr. DY Patil Institute of Technology - Dr. DY Patil Unitech Society, dpu"
+          content="acunetix, event, dy patil, acunetix10, acunetix-10, dyp, ACUNETIX,game, pimpri, pune,metaverse, vr, ar, , tech, acunetix tech, computer, party, Treasure Trove, UXplorer, Brainiac, DPL, Innovatia, ml ops, gamestorm, Asia,Technology,Science ,Scientific,Tech,Events ,Submerged,aquatic,journey,submarine,plants,Water,memories,Covid,festival,excitement,fantasty art,tech festival copenhangen,tech festival amsterdam,tech festival india,bombay techfest,mumbai,dates,march,9th - 11th,offline,online,Asia's largest science and technology festival,techfest app,Ocean,Multitute,spectacles,glasses,waterfall,volcanoes,deep,sea,giants,bioluminescent,beach,ancient,civilizations,Oceanography,Dr. Gene Feldman,NASA,Space,Mars,animals ,Earth,mystery,realm,sumberged ,submerged sojourn,deep sea,underwater,deep exploration,aquatic,underwater technology, dr dy patil institute of technology, iit, Dr. DY Patil Institute of Technology - Dr. DY Patil Unitech Society, dpu"
         />
         <meta name="robots" content="all" />
         <meta name="author" content="Burhanuddin Marcha" />
         <meta
           name="google-site-verification"
           content="EQPllv4yOfMZ3t9RXE0Uxqs8Rul432nBiJMab-lKYzI"
+        />
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={addProductJsonLd()}
+          key="product-jsonld"
         />
         <link rel="icon" type="image/png" href="/image/logo.png" />
         <link
